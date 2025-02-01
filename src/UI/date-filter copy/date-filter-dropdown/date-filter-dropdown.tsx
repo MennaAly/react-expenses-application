11e6b23d28c53a2d4c08a3f3715161c52669dcc7
MennaAly <mennaali365@gmail.com>
2025-02-01 15:39:35 -0500

    add date-filter-dropdown

---
+++ src/UI/date-filter copy/date-filter-dropdown/date-filter-dropdown.tsx
@@ -0,0 +1,39 @@
+import React, { Dispatch, SetStateAction, useState } from "react";
+import useCloseContainerWhenClickOutside from "../../../hooks/useCloseContainerWhenClickOutside";
+import { IDate } from "../../../interfaces/uiInterfaces";
+import DatePicker from "../../data-picker/data-picker";
+
+function DateFilterDropdown({title , selectedDate, setDate} : {title: string , selectedDate: IDate, setDate : Dispatch<SetStateAction<IDate>>}) {
+    const [isShowDatePicker, setIsShowDatePicker] = useState<boolean>();
+    const {containerRef} = useCloseContainerWhenClickOutside(setIsShowDatePicker);
+    function renderDatePicker() {
+        setIsShowDatePicker(true);
+    }
+
+    function toggleDatePickerView() {
+        if(isShowDatePicker) {
+            setIsShowDatePicker(false)
+        }
+        else {
+            renderDatePicker();
+        }
+    }
+    function onChangeDate(date: IDate) {
+        setDate(date);
+        setIsShowDatePicker(false);
+    }
+    
+    return(
+        <div className="date-filter__dropdown-container">
+            <span>{title}</span>
+            <button className="date-filter__dropdown" onClick={toggleDatePickerView}>
+                <span>{selectedDate.Year}</span>
+                <span>{selectedDate.Month}</span>
+                <span>{selectedDate.Day}</span>
+            </button>
+            {isShowDatePicker && <DatePicker ref={containerRef} onChangeDate={onChangeDate}/>}
+        </div>
+    )
+}
+
+export default DateFilterDropdown;
