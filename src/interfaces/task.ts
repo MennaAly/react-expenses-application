ccd79e4d5b165e7934bfb8aba6e3bc860a5fcfac
MennaAly <mennaali365@gmail.com>
2025-02-03 16:16:50 -0500

    remove task interfaces file

--- src/interfaces/task.ts
+++
@@ -1,8 +0,0 @@
-export interface DropdownProps {
-    value: string;
-    label: string;
-}
-
-export interface DropdownComponentProps {
-    options: DropdownProps[]; // options should be an array of DropdownProps
-}
