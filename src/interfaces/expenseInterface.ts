36c55c4180acc479188a31d603ed3c3e2fa8750c
MennaAly <mennaali365@gmail.com>
2025-02-25 09:49:13 -0500

    update form interfaces to match the validation

--- src/interfaces/expenseInterface.ts
+++ src/interfaces/expenseInterface.ts
@@ -1,10 +1,21 @@
 import { UseMutationResult } from "react-query"
 
+export interface IExpenseFormField {
+    value: string,
+    touched: boolean
+}
 export interface IExpense {
     id: string,
     title: string,
     amount: string,
-    date: string
+    date: string;
+}
+export interface IExpenseFormState {
+    id: string,
+    title: IExpenseFormField,
+    amount: IExpenseFormField,
+    date: IExpenseFormField;
+    isFormValid: boolean;
 }
 
 export interface IExpenseFormProps {
