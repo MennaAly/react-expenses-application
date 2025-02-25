7a201d3688e7b667ef660a347eeb33b45288fea1
MennaAly <mennaali365@gmail.com>
2025-02-25 09:48:08 -0500

    validate expense form to add red border when the value is empty and disable api call when the form isnot valid

--- src/expense-form/expense-form.tsx
+++ src/expense-form/expense-form.tsx
@@ -1,25 +1,21 @@
 import React, { useReducer } from "react";
 import { createPortal } from 'react-dom';
-import { IExpense, IExpenseFormProps, IExpenseFormState } from "../interfaces/expenseInterface";
+import { IExpenseFormField, IExpenseFormProps, IExpenseFormState } from "../interfaces/expenseInterface";
 import FormInput from "../UI/form-input/form-input";
 import "./expense-form.css";
 import { ReactSVG } from "react-svg";
 import crossIcon from "../assets/cross.svg";
+import { formInputs } from "../helper/constants/expense-form-constants";
+import { isValueEmpty, validateForm } from "../helper/utils/expense-form";
 
-function formReducer(state : IExpenseFormState, action : {type: string, payload: string | boolean}) {
+function formReducer(state : IExpenseFormState, action : {type: string, payload: string}) {
     switch(action.type) {
         case 'SET_TITLE':
-            return {...state, title: action.payload as string}
+            return {...state, title: {value: action.payload, touched: true}, isFormValid: validateForm(action.payload, state.amount.value, state.date.value)}
         case 'SET_AMOUNT':
-            return {...state, amount: action.payload as string}
+            return {...state, amount: {value: action.payload, touched: true} , isFormValid: validateForm(state.title.value, action.payload, state.date.value)}
         case 'SET_DATE':
-            return {...state, date: action.payload as string}
-        case 'SET_TITLE_ERROR':
-            return {...state, isTitleEmpty: action.payload as boolean}
-        case 'SET_AMOUNT_ERROR':
-            return {...state, isAmountEmpty: action.payload as boolean} 
-        case 'SET_DATE_ERROR':
-            return {...state, isDateEmpty: action.payload as boolean}
+            return {...state, date: {value: action.payload, touched: true}, isFormValid: validateForm(state.title.value, state.amount.value, action.payload)}
         default: 
             return state;
     }
@@ -28,30 +24,33 @@ function formReducer(state : IExpenseFormState, action : {type: string, payload:
 function ExpenseForm({action, expense: {id = '', title = '', amount = '', date = ''}, closeModal} : IExpenseFormProps) {
     const initialExpenseState: IExpenseFormState = {
         id,
-        title,
-        amount,
-        date,
-        isTitleEmpty: false,
-        isAmountEmpty: false,
-        isDateEmpty: false,
+        title : {
+            value: title,
+            touched: false
+        },
+        amount : {
+            value: amount,
+            touched: false
+        },
+        date: {
+            value: date,
+            touched: false
+        },
+        isFormValid: false
     };
     const [state, dispatch] = useReducer(formReducer, initialExpenseState);
-    function validateForm() {
-        dispatch({type: 'SET_TITLE_ERROR', payload: state.title.trim() == ''});
-        dispatch({type: 'SET_AMOUNT_ERROR', payload: state.amount.trim() == ''});
-        dispatch({type: 'SET_DATE_ERROR', payload: state.date.trim() == ''});
-        return state.title.trim() !== '' && state.amount.trim() !== '' && state.date.trim() !== '';
-    }
+
     function submitExpense(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
         event?.stopPropagation();
         event?.preventDefault();
-        if(validateForm()) {
-            action.mutationFn.mutate(state);
-            closeModal();
-        }
+        action.mutationFn.mutate({id: state.id, title: state.title.value, amount: state.amount.value, date: state.date.value });
+        closeModal();
+    }
+
+    function changeFormFieldValue(field: string, value: string) {
+        dispatch({type: `SET_${field.toUpperCase()}`, payload: value});
     }
     
-
     return createPortal((
         <>
             <div className="overlay"></div>
@@ -60,11 +59,12 @@ function ExpenseForm({action, expense: {id = '', title = '', amount = '', date =
                     <ReactSVG src={crossIcon} className="expense-form__close-icon" onClick={() => closeModal()}/>
                 </header>
                 <div className="expense-form___inputs-container">
-                    <FormInput label="Title" inputType="text" value={state.title} actionFn={(value) => {dispatch({type: 'SET_TITLE', payload: value})}} isInputEmpty={state.isTitleEmpty}/>
-                    <FormInput label="Amount" inputType="number" value={state.amount} actionFn={(value) => {dispatch({type: 'SET_AMOUNT', payload: value})}} isInputEmpty={state.isAmountEmpty}/>
-                    <FormInput label="Date" inputType="date" value={state.date} actionFn={(value) => {dispatch({type: 'SET_DATE', payload: value})}} isInputEmpty={state.isDateEmpty}/>
+                    {formInputs.map((fields) => {
+                        const {value , touched} = state[fields.name] as IExpenseFormField;
+                        return  <FormInput label={fields.name} inputType={fields.inputType} value={value} actionFn={(value) => {changeFormFieldValue(fields.name, value)}} isInputEmpty={touched && isValueEmpty(value)}/>
+                    })}
                 </div>
-                <button className="expense-form__call-to-action" onClick={(event) => submitExpense(event)}>{action.name}</button>
+                <button className={`expense-form__call-to-action ${!state.isFormValid ? 'expense-form__call-to-action--disabled' : null}`} disabled={state.isFormValid} onClick={(event) => submitExpense(event)}>{action.name}</button>
             </form>
         </>
     ), document.getElementById('modal-root') as HTMLElement);
